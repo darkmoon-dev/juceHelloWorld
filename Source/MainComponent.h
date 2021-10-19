7690df5a97b10820ebd6979e332aedf6afe875b9
darkmoon19 <anson.komla@outlook.fr>
2021-10-19 19:49:01 +0000

    minimal

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -21,7 +21,5 @@ public:
 private:
     //==============================================================================
     // Your private member variables go here...
-
-
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
